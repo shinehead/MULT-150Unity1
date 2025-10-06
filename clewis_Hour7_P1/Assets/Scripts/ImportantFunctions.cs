d6c01eed6555d43386dec035e37449c8b9fe72af
shinehead <118337403+shinehead@users.noreply.github.com>
2025-10-05 20:39:21 -0700

    Hour 7

---
+++ clewis_Hour7_P1/Assets/Scripts/ImportantFunctions.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImportantFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Start runs before an object Updates");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("This is called once a frame");
+    }
+}
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int runSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
