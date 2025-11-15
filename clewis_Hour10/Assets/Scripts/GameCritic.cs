e24047df1465bb68e80f80dce58a3523591b694b
shinehead <118337403+shinehead@users.noreply.github.com>
2025-11-15 03:55:39 -0800

    Hour10

---
+++ clewis_Hour10/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,7 @@
+// The game is Too easy.
+//Hitting the wrong light with the wrong colored ball could result in a temporary outtage of the light and prevent the player from eliminate that colored ball for a minute
+//If there were a sound for eliminating each ball it would have more of a wow factor.
+//The fun part of the game is watching the balls fly around. The game ends to quickly to be tedious.
+//Enhancemnent suggestions implemented: covered water outside of walls
+//Changed the shape of the bumper to a cylinder
+//Changed one of the chaos balls velocity to 40 and one to 70
