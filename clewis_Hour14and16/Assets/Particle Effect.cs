cd1896b222f095e94023acc84f908952db57180a
shinehead <118337403+shinehead@users.noreply.github.com>
2025-11-30 10:26:09 -0800

    Hour14and16

---
+++ clewis_Hour14and16/Assets/Particle Effect.cs
@@ -0,0 +1,5 @@
+// 1. changed the color of the particles 
+//2. Checked velocity over lifetime and changed the speed modifier to 1.01
+//3. Changed the start color to 9D62FB
+//4. Changed the start size to 0.15
+//5. Changed the start speed to 15
