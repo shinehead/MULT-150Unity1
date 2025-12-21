35a793334d1e1a5518da4ca2824b4d37b9927d18
shinehead <118337403+shinehead@users.noreply.github.com>
2025-12-20 20:19:16 -0800

    Hour 20

--- clewis_Hour20/Assets/Scripts/GameCritic.cs
+++ clewis_Hour20/Assets/Scripts/GameCritic.cs
@@ -1,6 +1,14 @@
 
    // Is the game too easy or hard?
+   //This game probably would be considered too easy by most people, but I find this type of casual game to be pretty fun even though there isn´t much to it.
    // What changes would you do to make it easier or harder?
+  //I think I might just add a few more powerups that did more than extend time, like short term invincibility, and obstacle destruction. I would add a point system and point buffs. 
+  //I might add an obstacle that the player has to duck under or jump over. I´d increase the speed of the obstacles and powerups gradually overtime, naybe even institute levels similar to tetris.
    // What would make the game more appealing?
+   //Engaging audio, and maybe some spectacle when powerups are acquired or a new highscore is reached to give the player more of a sense of accomplishment
    // What parts of the game are fun? 
+   // The challenge of simply dodging the obstacle while trying to get a 100% of the pickups is enough to get me engaged. I like games where the possibility of getting 100% of something is possible..
    // What parts of the game are tedious?
+   //It doesn´t really have any cutoff of achievement as it is, but that could easily be remedied with some of the things I mentioned.
+
+   //I made the powerup and obstacle texture, and changed the shape of the obstacle and powerup
